25d89fbd9f10cad8ded7ec6b56bd52c9bd688911
Zahid <zahidmohammad495@gmai.com>
2024-03-13 12:43:08 +0530

    add insert function

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      username: true,
+    }
+  });
+  console.log(res);
+}
+
+insertUser("kirat@gmail.com", "123456", "harkirat", "singh");
